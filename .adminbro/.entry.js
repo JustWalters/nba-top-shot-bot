f723f682d75632edaffbeb0b6a37217e60e6354d
Justin Walters <justin.walters@boweryvaluation.com>
2022-01-16 19:20:16 -0500

    Don't check in .adminbro/.entry.js

--- .adminbro/.entry.js
+++
@@ -1,5 +0,0 @@
-AdminBro.UserComponents = {}
-import Component1 from '../src/server/admin-components/MomentViewer'
-AdminBro.UserComponents.Component1 = Component1
-import Component2 from '../src/server/admin-components/AddMomentAndAlert'
-AdminBro.UserComponents.Component2 = Component2
